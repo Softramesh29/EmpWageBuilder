1d045c6b0967a9622aa16b949cd00f8eed724e94
softramesh29 <softramesh29@gmail.com>
2022-04-13 22:48:05 +0530

    Calculation of employee daily wages

---
+++ EmpDailyWage.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.EmpDailyWage;
+
+public class EmpDailyWage {
+	public static void main(String[] args) {
+		int is_full_time = 1;
+		int emp_rate_per_hour = 20;
+		int emphrs = 0;
+		int empwage = 0;
+
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+	if (empcheck == is_full_time)
+		emphrs = 8;
+		else
+		emphrs = 0;
+	empwage = emphrs * emp_rate_per_hour;
+	System.out.println("Employee wage : " + empwage);
+	}
+}
