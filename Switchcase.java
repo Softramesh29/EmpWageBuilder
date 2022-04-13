c79c8621cea7b757713e45190bcf4ecc9dd8742b
softramesh29 <softramesh29@gmail.com>
2022-04-13 22:57:39 +0530

    Using switch case

---
+++ Switchcase.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.Switchcase;
+
+public class Switchcase {
+	public static final int is_full_time = 2;
+	public static final int is_part_time = 1;
+	public static final int emp_rate_per_hour = 20;
+	
+	public static void main(String[] args) {
+
+		int emphrs = 0;
+		int empwage = 0;
+
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+		case is_full_time:
+		emphrs = 8;
+		break;
+		case is_part_time:
+		emphrs = 4;
+		break;
+		default:
+		emphrs = 0;
+		}	
+	empwage = emphrs * emp_rate_per_hour;
+	System.out.println("Employee wage : " + empwage);
+	}
+	}
+
